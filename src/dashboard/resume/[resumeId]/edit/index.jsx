8279ffbf40f7d650a02bf78d4dcdd5faef380d90
Piyush Gupta <guptapiyush315@gmail.com>
2024-07-06 13:31:39 +0530

    resume page

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,10 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import FormSection from '../../components/FormSection';
+import ResumePreview from '../../components/ResumePreview';
+import { ResumeInfoContext } from '@/context/ResumeInfoContext';
+import dummy from '@/data/dummy';
+import GlobalApi from '../../../../../services/GlobalApi';
 
-const index = () => {
+function EditResume() {
+    const {resumeId}=useParams();
+    const [resumeInfo,setResumeInfo]=useState();
+    useEffect(()=>{
+       
+        GetResumeInfo();
+    },[])
+
+
+    const GetResumeInfo=()=>{
+        GlobalApi.GetResumeById(resumeId).then(resp=>{
+          console.log(resp.data.data);
+          setResumeInfo(resp.data.data);
+        })
+    }
+
   return (
-    <div>index</div>
+    <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
+    <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
+        {/* Form Section  */}
+          <FormSection/>
+        {/* Preview Section  */}
+         <ResumePreview/>
+    </div>
+    </ResumeInfoContext.Provider>
   )
 }
 
-export default index
+export default EditResume
---
+++ src/dashboard/resume/components/FormSection.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const FormSection = () => {
+  return (
+    <div>FormSection</div>
+  )
+}
+
+export default FormSection
